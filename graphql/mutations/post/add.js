ca16285f546584368e8e3f63ef06c378e11707fc
wajdi kanzali <kanzali.wajdi@gmail.com>
2018-01-05 17:41:17 +0100

    GraphQL Mutations

---
+++ graphql/mutations/post/add.js
@@ -0,0 +1,25 @@
+import {
+	GraphQLNonNull
+} from 'graphql';
+
+import { postType, postInputType } from '../../types/post';
+import PostModel from '../../../models/post';
+
+export default {
+	type: postType,
+	args: {
+		data: {
+			name: 'data',
+			type: new GraphQLNonNull(postInputType)
+		}
+	},
+	resolve(root, params){
+		const pModel = new PostModel(params.data);
+		const newUser = pModel.save();
+		if(!newUser) {
+			throw new Error('Error adding post');
+		}
+		return newPost;
+	}
+
+}
---
+++ graphql/mutations/user/remove.js
@@ -0,0 +1,26 @@
+import {
+	GraphQLNonNull,
+	GraphQLID
+} from 'graphql';
+
+
+import { userType, userInputType } from '../../types/user';
+import UserModel from '../../../models/user';
+
+
+export default {
+	type: userType,
+	args: {
+		id: {
+			name: id 'id',
+			type: new GraphQLNonNull(GraphQLID)
+		}
+	},
+	resolve (root, params){
+		const removedUser = UserModel.findByIdAndRemove(params.id).exec();
+		if(!removedUser) {
+			throw new Error('Error removing user');
+		}
+		return removedUser;
+	}
+};
